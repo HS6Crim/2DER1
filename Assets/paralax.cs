4775ff2a2227f7985b3e725dd35b3914c750b527
HS6Crim <Hstraiton-miah2024@elam.co.uk>
2024-10-02 12:48:12 +0100

    New scene

--- Assets/paralax.cs
+++ Assets/paralax.cs
@@ -13,7 +13,7 @@ public class paralax : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("char001"); // find the player
+        player = GameObject.Find("Player"); // find the player
         rend = GetComponent<Renderer>(); // find the renderer
         playerStartPos = player.transform.position.x; // Save our start pos
     }
